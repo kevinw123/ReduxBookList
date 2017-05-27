e569823253acf70dc796b9b22fa9dfbc88adc706
Kevin Wong <kevinwong_895@hotmail.com>
2017-05-27 11:55:16 -0400

    Editing style

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -4,10 +4,10 @@ import { connect } from 'react-redux';
 class BookDetail extends Component {
   render(){
     if(!this.props.book){
-      return <div>Select a book to get started.</div>
+      return <div className="book-detail">Select a book to get started.</div>
     }
     return (
-      <div>
+      <div className="book-detail">
         <h3>Details for:</h3>
         <div>Title: {this.props.book.title}</div>
         <div>Pages: {this.props.book.pages}</div>
